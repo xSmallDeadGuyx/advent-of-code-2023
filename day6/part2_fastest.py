117bcc946043d699d11b1bde56aeafa936dfbcd1
Alex Gilday <alex.gilday@gmail.com>
2023-12-06 09:53:04 +0000

    Day 6 part2_fastest further optimization and logic fix

--- day6/part2_fastest.py
+++ day6/part2_fastest.py
@@ -14,16 +14,16 @@ dist = int(''.join(lines[1].split()[1:]))
 # x * (time - x) = distance
 # -x^2 + time*x - distance = 0
 
-# Quadratic equation x = (-b +- sqrt(sqr(b) - 4ac)) / 2a
+# Quadratic equation x = (-b +- sqrt(b^2 - 4ac)) / 2a
+#   a = -1 so:
+# x = (-b +- sqrt(b^2 - 4*-1*c)) / (2 * -1) = (b +- sqrt(b^2 + 4c))/2
 
-a = -1
 b = time
 c = -dist
 
-x = (-b + math.sqrt(b ** 2 - 4 * a * c)) / (2 * a)
+x = (b - math.sqrt(b**2 + 4 * c)) / 2
 
-min_winning_time = math.ceil(x)
-min_losing_time = min_winning_time - 1
+min_losing_time = math.floor(x)
 
 total_losing_times = min_losing_time * 2 + 1 # Account for time * 0
 
